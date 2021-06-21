29b6de78a06faa57b604a9e413628418811162fb
Theodoreleonardo <theodore.leonardo@student.umn.ac.id>
2021-06-21 23:08:03 +0700

    ad

--- apotek(9).sql
+++ apotek(9).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2021 at 05:48 PM
+-- Generation Time: Jun 21, 2021 at 06:07 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.18
 
@@ -50,7 +50,7 @@ CREATE TABLE `tbl_barang` (
 --
 
 INSERT INTO `tbl_barang` (`id_barang`, `id_item`, `id_kategori`, `id_supplier`, `id_brand`, `kode_barang`, `nama_barang`, `stock_barang`, `gambar_barang`, `qr_code`, `deskripsi`, `harga_barang`, `tempo_barang`, `create_date`, `status`) VALUES
-(12, '09254620210528', 6, 7, 7, '23443fds', 'asd', 22, '09254620210528asd1.png', '3121231', 'sadasd asdasd asd asd as', 12000, '2021-06-07', '2021-05-28', 'ACTIVE'),
+(12, '09254620210528', 6, 7, 7, '23443fds', 'asd', 20, '09254620210528asd1.png', '3121231', 'sadasd asdasd asd asd as', 12000, '2021-06-07', '2021-05-28', 'ACTIVE'),
 (13, '10341120210528', 6, 7, 6, '231', 'andi', 123, '10341120210528sandhika.jpeg', '3121231', 'ayam ayam', 10000000, '2021-06-04', '2021-04-15', 'ACTIVE'),
 (14, '07144620210530', 6, 7, 7, '123', 'dinda', 120, '08512620210606test1.png', '3121231', 'sdfdsfsdf', 2000, '2021-05-30', '2021-05-30', 'ACTIVE'),
 (15, '12512720210616', 6, 7, 7, '231', 'anjayy', 15, '12512720210616Untitled-2.png', '3121231', 'kebesaran', 13000, '2021-06-17', '2021-06-16', 'IN-ACTIVE'),
@@ -83,7 +83,8 @@ INSERT INTO `tbl_barang_keluar` (`id_keluar`, `id_petugas`, `id_item`, `jumlah_b
 (16, 1, '10341120210528', 1, 'adas', 'Barang Keluar', 124, '2021-06-17'),
 (17, 1, '12535120210616', 2, 'ads', 'Barang Keluar', 10, '2021-06-17'),
 (18, 1, '10341120210528', 3, 'asddsadsa', 'Barang Keluar', 121, '2021-06-19'),
-(19, 1, '07144620210530', 3, 'asd', 'Barang Keluar', 120, '2021-06-19');
+(19, 1, '07144620210530', 3, 'asd', 'Barang Keluar', 120, '2021-06-19'),
+(20, 1, '09254620210528', 5, 'asd ads', 'Salah Input', 20, '2021-06-21');
 
 -- --------------------------------------------------------
 
@@ -99,6 +100,7 @@ CREATE TABLE `tbl_barang_masuk` (
   `sisah_stock` int(11) NOT NULL,
   `no_faktur` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
   `create_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -106,12 +108,13 @@ CREATE TABLE `tbl_barang_masuk` (
 -- Dumping data for table `tbl_barang_masuk`
 --
 
-INSERT INTO `tbl_barang_masuk` (`id_masuk`, `id_item`, `id_petugas`, `jumlah_barang`, `sisah_stock`, `no_faktur`, `status`, `create_date`) VALUES
-(22, '09254620210528', 1, 1, 6, 'asdadsd adas', 'Barang Masuk', '2021-06-01'),
-(23, '10341120210528', 1, 2, 123, 'asdadsd adas', 'Barang Masuk', '2021-06-01'),
-(24, '07144620210530', 1, 1, 121, 'asd213', 'Barang Masuk', '2021-06-17'),
-(25, '07144620210530', 1, 2, 123, 'asdasd', 'Barang Masuk', '2021-06-17'),
-(26, '09254620210528', 1, 20, 22, '234432', 'Barang Masuk', '2021-06-19');
+INSERT INTO `tbl_barang_masuk` (`id_masuk`, `id_item`, `id_petugas`, `jumlah_barang`, `sisah_stock`, `no_faktur`, `status`, `keterangan`, `create_date`) VALUES
+(22, '09254620210528', 1, 1, 6, 'asdadsd adas', 'Barang Masuk', '', '2021-06-01'),
+(23, '10341120210528', 1, 2, 123, 'asdadsd adas', 'Barang Masuk', '', '2021-06-01'),
+(24, '07144620210530', 1, 1, 121, 'asd213', 'Barang Masuk', '', '2021-06-17'),
+(25, '07144620210530', 1, 2, 123, 'asdasd', 'Barang Masuk', '', '2021-06-17'),
+(26, '09254620210528', 1, 20, 22, '234432', 'Barang Masuk', '', '2021-06-19'),
+(27, '09254620210528', 1, 3, 25, '21das', 'Barang Masuk', 'asdsda asdads', '2021-06-21');
 
 -- --------------------------------------------------------
 
@@ -225,10 +228,10 @@ CREATE TABLE `tbl_pesan` (
 --
 
 INSERT INTO `tbl_pesan` (`id_pesan`, `id_barang`, `nama_barang`, `img`, `stock`, `desc`, `create_date`) VALUES
-(1, 16, 'anjayy', '12535120210616Untitled-2.png', 10, 'Stock barang hampir habis', '2021-06-20'),
-(2, 12, 'asd', '09254620210528asd1.png', 22, 'Barang Kadaluarsa', '2021-06-20'),
-(3, 13, 'andi', '10341120210528sandhika.jpeg', 123, 'Barang Kadaluarsa', '2021-06-20'),
-(4, 14, 'dinda', '08512620210606test1.png', 120, 'Barang Kadaluarsa', '2021-06-20');
+(1, 16, 'anjayy', '12535120210616Untitled-2.png', 10, 'Stock barang hampir habis', '2021-06-21'),
+(2, 12, 'asd', '09254620210528asd1.png', 20, 'Barang Kadaluarsa', '2021-06-21'),
+(3, 13, 'andi', '10341120210528sandhika.jpeg', 123, 'Barang Kadaluarsa', '2021-06-21'),
+(4, 14, 'dinda', '08512620210606test1.png', 120, 'Barang Kadaluarsa', '2021-06-21');
 
 -- --------------------------------------------------------
 
@@ -375,13 +378,13 @@ ALTER TABLE `tbl_barang`
 -- AUTO_INCREMENT for table `tbl_barang_keluar`
 --
 ALTER TABLE `tbl_barang_keluar`
-  MODIFY `id_keluar` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_keluar` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `tbl_barang_masuk`
 --
 ALTER TABLE `tbl_barang_masuk`
-  MODIFY `id_masuk` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_masuk` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `tbl_brand`
